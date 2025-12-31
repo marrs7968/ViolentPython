1f6b38bf9c658c4e1bf116c0a5e2f72eb2e3c069
Srira <sr7968@nyu.edu>
2025-12-31 09:33:21 -0500

    testPass function complete, needed to use passlib library with des_crypt function because crypt function is deprecated

--- UNIXCrack.py
+++ UNIXCrack.py
@@ -1,5 +1,17 @@
 import cryptography
+from passlib.hash import des_crypt # crypt is deprecated
 
 def testPass(cryptPass):
     salt = cryptPass[0:2]
+    dictFile = open('dictionary.txt', 'r')
+    for word in dictFile.readlines():
+        word = word.strip('\n')
+        cryptWord = des_crypt.using(salt=salt).hash(word)
+        if (cryptWord == cryptPass):
+            print(f"[+] Found Password: {word}\n")
+            return
+    print("[-] Password Not Found.\n")
+    return
+
+def main():
     
