8dda115d46601a542a9fe051cf6a21a8ed5c5e6b
Srira <sr7968@nyu.edu>
2026-01-03 15:40:15 -0500

    Project 2: TCP Port Scanner, added connScan and portScan functions. connScan() attempts connection for a given port, and portScan resolves a given hostname, and then probes each port from the provided list of ports

--- ch2/TCPPortScanner.py
+++ ch2/TCPPortScanner.py
@@ -1,7 +1,8 @@
 import optparse
+from socket import *
 
 # PARSING OUR TARGET HOSTNAME AND PORT TO SCAN
-parser = optparse.OptionParser('usaage %prog -H <target host> -p <target port>')
+parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
 parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
 parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
 (options, args) = parser.parse_args()
@@ -12,3 +13,31 @@ if (tgtHost == None) or (tgtPort == None):
     print(parser.usage)
     exit(0)
 
+# ENUMERATING THROUGH EACH PORT AND ATTEMPTING CONNECTION
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+        connSkt.connect((tgtHost, tgtPort))
+        print(f"[+]{tgtPort}/tcp open")
+        connSkt.close()
+    except:
+        print(f"[-]{tgtPort}/tcp closed")
+
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print(f"[-] Cannot resolve {tgtHost}: Unknown host")
+        return
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print(f"\n[+] Scan Results for: {tgtName[0]}")
+        return
+    except:
+        print(f"\n[+] Scan Results for: {tgtIP}")
+    setdefaulttimeout(1)
+
+    for tgtPort in tgtPorts:
+        print(f"Scanning port {tgtPort}")
+        connScan(tgtHost, int(tgtPort)
+                 )
