82f6668cddb61c81c354aeca63617961d71c5856
Srira <sr7968@nyu.edu>
2026-01-03 16:11:12 -0500

    made a few fixes to port scanner, thanks chatGPT. There were some issues caused by Python 2 (what the book was written in) and Python 3 discrepancies. Also not wrapping port list parameter in quotes gives Powershell some trouble. Seems to be working now though

--- ch2/TCPPortScanner.py
+++ ch2/TCPPortScanner.py
@@ -1,28 +1,21 @@
 import optparse
 from socket import *
 
-# PARSING OUR TARGET HOSTNAME AND PORT TO SCAN
-parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
-parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
-(options, args) = parser.parse_args()
-
-tgtHost = options.tgtHost
-tgtPort = options.tgtPort
-if (tgtHost == None) or (tgtPort == None):
-    print(parser.usage)
-    exit(0)
-
-# ENUMERATING THROUGH EACH PORT AND ATTEMPTING CONNECTION
+# ATTEMPTING CONNECTION FOR PROVIDED HOST AND PORT
 def connScan(tgtHost, tgtPort):
     try:
         connSkt = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
         connSkt.connect((tgtHost, tgtPort))
+        connSkt.send(b"C3RT1F13D_SKR1PTK1DD13")
+
+        results = connSkt.recv(100)
         print(f"[+]{tgtPort}/tcp open")
+        print(f"[+] {str(results)}")
         connSkt.close()
     except:
         print(f"[-]{tgtPort}/tcp closed")
 
+# RESOLVING HOSTNAME AND ENUMERATING THROUGH EACH PORT FROM PROVIDED PORT LIST
 def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
@@ -32,12 +25,27 @@ def portScan(tgtHost, tgtPorts):
     try:
         tgtName = gethostbyaddr(tgtIP)
         print(f"\n[+] Scan Results for: {tgtName[0]}")
-        return
     except:
         print(f"\n[+] Scan Results for: {tgtIP}")
     setdefaulttimeout(1)
 
     for tgtPort in tgtPorts:
         print(f"Scanning port {tgtPort}")
-        connScan(tgtHost, int(tgtPort)
-                 )
+        connScan(tgtHost, int(tgtPort))
+
+def main():
+    # PARSING OUR TARGET HOSTNAME AND PORT TO SCAN
+    parser = optparse.OptionParser('usage%prog -H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
+    (options, args) = parser.parse_args()
+
+    tgtHost = options.tgtHost
+    tgtPorts = [p.strip() for p in options.tgtPort.split(',') if p.strip()]
+    if (tgtHost == None) or (tgtPorts[0] == None):
+        print(parser.usage)
+        exit(0)
+    portScan(tgtHost, tgtPorts)
+
+if __name__ == '__main__':
+    main()
